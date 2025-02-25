36f84257f300b51277657d2ef7ec3708ba41001b
Dein Agiobu <agiobudein@gmail.com>
2025-02-25 12:20:55 +0100

    second msg

---
+++ src/test/java/com/testcode/qrCodeGeneration/QrCodeGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcode.qrCodeGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
